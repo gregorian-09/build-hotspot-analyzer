bf1afbf0155e869aad8384e31b50be01bfbd8246
Gregorian Rayne <gregorianrayne09@gmail.com>
2025-10-16 18:13:41 +0100

    build: add include directory for graph bulder module

---
+++ headers/bha/graph/graph_builder.h
@@ -0,0 +1,8 @@
+//
+// Created by grego on 16/10/2025.
+//
+
+#ifndef GRAPH_BUILDER_H
+#define GRAPH_BUILDER_H
+
+#endif //GRAPH_BUILDER_H
---
+++ sources/bha/graph/graph_builder.cpp
@@ -0,0 +1,3 @@
+//
+// Created by grego on 16/10/2025.
+//
